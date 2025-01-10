e1dfd7414b1b1a3ae8c645b8123c92ea68d6ca08
Nathan5563 <ndawit611@gmail.com>
2025-01-10 03:23:22 -0500

    Added credit

--- Components/font.h
+++ Components/font.h
@@ -1,7 +1,7 @@
 #ifndef __FONT_H__
 #define __FONT_H__
 
-// Font taken from https://tobiasvl.github.io/blog/write-a-chip-8-emulator/#prerequisites
+// Font taken from https://tobiasvl.github.io/blog/write-a-chip-8-emulator/
 
 #include "types.h"
 
