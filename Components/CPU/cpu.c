2546bf76b1befbeed0a868a2ef56e7e9b42d83f4
Nathan5563 <ndawit611@gmail.com>
2025-01-11 11:13:11 -0500

    Implemented most unambiguous no-input-required instructions

--- Components/CPU/cpu.c
+++ Components/CPU/cpu.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <unistd.h>
+#include <time.h>
 
 #include "cpu.h"
 #include "../Display/display.h"
@@ -10,6 +11,8 @@
 #include "../font.h"
 
 #define FONT_SIZE 80
+
+// TODO: Use lots of macros to prevent hex confusion
 
 struct _cpu
 {
@@ -21,18 +24,10 @@ struct _cpu
     bool drawn;
 };
 
-void *copy(const void *src) {
-    if (!src) return NULL;
-    WORD *copy = malloc(sizeof(WORD));
-    if (!copy) return NULL;
-    *copy = *(const WORD *)src;
-    return copy;
-}
-
 cpu* cpu_init()
 {
     cpu* c = malloc(sizeof(cpu));
-    c->Stack = stack_create(copy);
+    c->Stack = stack_create();
     c->PC = 0x0200;
     c->I = 0x0000;
     c->Delay = 0x00;
@@ -72,34 +67,151 @@ WORD cpu_fetch(cpu* c)
 void cpu_exec(cpu* c, WORD ins, bool quit, SDL_Window** window, SDL_Surface** surface)
 {
     c->drawn = false;
-    int reg;
-    int x, y, height;
+    int X, Y, N;
     switch(ins & 0xF000)
     {
         case 0x0000:
-            clear_screen();
+            switch(ins & 0x00FF)
+            {
+                case 0x00E0:
+                    clear_screen();
+                    break;
+                case 0x00EE:
+                    c->PC = stack_pop(c->Stack);
+                    break;
+            }
             break;
         case 0x1000:
             c->PC = (ins & 0x0FFF);
             break;
+        case 0x2000:
+            stack_push(c->Stack, c->PC);
+            c->PC = (ins & 0x0FFF);
+            break;
+        case 0x3000:
+            X = ((ins & 0x0F00) >> 8);
+            if (c->V[X] == (ins & 0x00FF)) c->PC += 2;
+            break;
+        case 0x4000:
+            X = ((ins & 0x0F00) >> 8);
+            if (c->V[X] != (ins & 0x00FF)) c->PC += 2;
+            break;
+        case 0x5000:
+            X = ((ins & 0x0F00) >> 8);
+            Y = ((ins & 0x00F0) >> 4);
+            if (c->V[X] == c->V[Y]) c->PC += 2;
+            break;
         case 0x6000:
-            reg = ((ins & 0x0F00) >> 8);
-            c->V[reg] = (ins & 0x00FF);
+            X = ((ins & 0x0F00) >> 8);
+            c->V[X] = (ins & 0x00FF);
             break;
         case 0x7000:
-            reg = ((ins & 0x0F00) >> 8);
-            c->V[reg] += (ins & 0x00FF);
+            X = ((ins & 0x0F00) >> 8);
+            c->V[X] += (ins & 0x00FF);
+            break;
+        case 0x8000:
+            X = ((ins & 0x0F00) >> 8);
+            Y = ((ins & 0x00F0) >> 4);
+            switch(ins & 0x000F)
+            {
+                case 0x0000:
+                    c->V[X] = c->V[Y];
+                    break;
+                case 0x0001:
+                    c->V[X] |= c->V[Y];
+                    break;
+                case 0x0002:
+                    c->V[X] &= c->V[Y];
+                    break;
+                case 0x0003:
+                    c->V[X] ^= c->V[Y];
+                    break;
+                case 0x0004:
+                    c->V[X] += c->V[Y];
+                    if (c->V[X] + c->V[Y] >= 0x100) c->V[0xF] = 1;
+                    else c->V[0xF] = 0;
+                    break;
+                case 0x0005:
+                    c->V[X] -= c->V[Y];
+                    if (c->V[X] > c->V[Y]) c->V[0xF] = 1;
+                    else c->V[0xF] = 0;
+                    break;
+                case 0x0006:
+                    // Ambiguous
+                    break;
+                case 0x0007:
+                    c->V[X] = c->V[Y] - c->V[X];
+                    if (c->V[Y] > c->V[X]) c->V[0xF] = 1;
+                    else c->V[0xF] = 0;
+                    break;
+                case 0x000E:
+                    // Ambiguous
+                    break;
+            }
+        case 0x9000:
+            X = ((ins & 0x0F00) >> 8);
+            Y = ((ins & 0x00F0) >> 4);
+            if (c->V[X] != c->V[Y]) c->PC += 2;
             break;
         case 0xA000:
             c->I = (ins & 0x0FFF);
             break;
+        case 0xB000:
+            // Ambiguous
+            break;
+        case 0xC000:
+            X = ((ins & 0x0F00) >> 8);
+            c->V[X] = (rand() % (0xFF - 0x00 + 1) + 0x00) & (ins & 0x00FF);
         case 0xD000:
             c->drawn = true;
-            x = ((ins & 0x0F00) >> 8);
-            y = ((ins & 0x00F0) >> 4);
-            height = (ins & 0x000F);
-            draw(c, window, surface, c->V[x], c->V[y], height);
+            X = ((ins & 0x0F00) >> 8);
+            Y = ((ins & 0x00F0) >> 4);
+            N = (ins & 0x000F);
+            draw(c, window, surface, c->V[X], c->V[Y], N);
             break;
+        case 0xE000:
+            X = ((ins & 0x0F00) >> 8);
+            switch(ins & 0x00FF)
+            {
+                case 0x009E:
+                    break;
+                case 0x00A1:
+                    break;
+            }
+        case 0xF000:
+            X = ((ins & 0x0F00) >> 8);
+            switch(ins & 0x00FF)
+            {
+                case 0x0007:
+                    c->V[X] = c->Delay;
+                    break;
+                case 0x000A:
+                    
+                    break;
+                case 0x0015:
+                    c->Delay = c->V[X];
+                    break;
+                case 0x0018:
+                    c->Sound = c->V[X];
+                    break;
+                case 0x001E:
+                    c->I += c->V[X];
+                    if (c->I + c->V[X] >= 0x1000) c->V[0xF] = 1;
+                    // might need to set VF = 0 otherwise, but leave as is for now
+                    break;
+                case 0x0029:
+                    // set I to the address where the character represented by the
+                    // last nibble of VX is stored in the font (in memory)
+                    break;
+                case 0x0033:
+                    break;
+                case 0x0055:
+                    // Ambiguous
+                    break;
+                case 0x0065:
+                    // Ambiguous
+                    break;
+            }
         default:
             quit = true;
             break;
